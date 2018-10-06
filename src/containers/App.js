00755d54277fa3ab8c728f5f91fe4ed2a3c8512d
Tim <i_timour@hotmail.com>
2018-10-06 17:49:22 -0400

    First commit

---
+++ src/containers/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll';
+import './App.css';
+
+import { setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: []            
+        }
+    }
+
+    componentDidMount() {        
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            return response.json();
+        })
+        .then(users => {
+            this.setState({ robots: users })
+        });
+    }
+
+    render() {
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        if(!robots.length) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>                
+                </div>                
+            );
+        }        
+    }    
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
